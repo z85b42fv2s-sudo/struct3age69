e7bef7915d3b69b1094d7d15d2a2ab51acd06542
Fabrizio Marrone <medicalyze.service.ita@outlook.com>
2025-12-30 15:38:05 +0100

    Meta tag Google Search Console con st.components.v1.html per verifica sito

--- app.py
+++ app.py
@@ -56,18 +56,22 @@ STRIPE_PUBLIC_KEY = "mk_1ShT5mAHjVSlqjiBcdK8asiZ"
 STRIPE_SECRET_KEY = "mk_1ShT6iAHjVSlqjiBN9zJb2tO"
 
 
+
 st.set_page_config(
     page_title="Structure3Age",
     page_icon="🧱",
     layout="wide"
 )
 
-# Meta tag Google Search Console
-st.markdown(
+# Meta tag Google Search Console (iniettato con component)
+import streamlit.components.v1 as components
+components.html(
     """
-    <meta name="google-site-verification" content="D-2hzr1YwD7pGFy_0r30wH3TuUFqkSpmXooJbX0_RlI" />
+    <head>
+        <meta name=\"google-site-verification\" content=\"D-2hzr1YwD7pGFy_0r30wH3TuUFqkSpmXooJbX0_RlI\" />
+    </head>
     """,
-    unsafe_allow_html=True
+    height=0
 )
 
 # --- AUTENTICAZIONE UTENTE ---
